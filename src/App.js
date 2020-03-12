aeb36ea22d8736e12153eefd4e00b3547f0f164f
jaren <jarensmart@yahoo.com>
2020-03-11 21:40:36 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
+
     <Navigation />
   </div>
 );
